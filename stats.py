5c084e1b83ce93e88792e4faee471efc387c2cc2
Poorya <poorya.phm716@gmail.com>
2026-08-16 12:47:43 +0330

    adding python stats

--- stats.py
+++ stats.py
@@ -1,1 +1,3 @@
-just a place holder
+#write a stats collector in python
+#the stats collector should collect statsfrom the system and store them in a database
+#the stats collector should be able to collect stats from the system and store them in a database
